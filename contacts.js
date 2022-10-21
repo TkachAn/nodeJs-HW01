79f1829b77c8d4581274768169f01f6520324756
TkachAn <4ndrey.tkachenko@gmail.com>
2022-10-21 14:53:34 +0300

    Update contacts.js

--- contacts.js
+++ contacts.js
@@ -1,4 +1,29 @@
-const fs = require("fs");
+const fs = require("fs").promises;
 const path = require("path");
 const contactsPath = path.resolve("contacts.json");
 console.log("contactsPath:", contactsPath);
+
+/*
+ * Раскомментируй и запиши значение
+ * const contactsPath = ;
+ */
+
+// // TODO: задокументировать каждую функцию
+function listContacts() {
+  fs.readFile(Path, utf8)
+    .then((data) => console.log(toString(data)))
+    .catch((err) => console.error(err));
+}
+
+// function getContactById(contactId) {
+//   // ...твой код
+// }
+
+// function removeContact(contactId) {
+//   // ...твой код
+// }
+
+// function addContact(name, email, phone) {
+//   // ...твой код
+// }
+// module.exports(listContacts, getContactById, addContact, removeContact);
