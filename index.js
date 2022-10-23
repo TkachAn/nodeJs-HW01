23d9c1694a7dda6b959f3db463e2c54be3e78076
TkachAn <4ndrey.tkachenko@gmail.com>
2022-10-23 17:41:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,64 @@
+const chalk = require("chalk");
+const {Command} = require("commander");
+
+const program = new Command();
+
 const {
-  listContacts,
+  listContact,
   getContactById,
-  addContact,
   removeContact,
+  addContact,
 } = require("./contacts");
-// const argv = require("yargs").argv;
-// console.log(listContacts, getContactById, addContact, removeContact);
-// // TODO: рефакторить
-// function invokeAction({action, id, name, email, phone}) {
-//   switch (action) {
-//     case "list":
-//       listContacts();
-//       break;
 
-//     case "get":
-//       getContactById(id);
-//       break;
+program
+  .requiredOption("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
 
-//     case "add":
-//       addContact(name, email, phone);
-//       break;
+program.parse(process.argv);
 
-//     case "remove":
-//       removeContact(id);
-//       break;
+const argv = program.opts();
+console.log("argv:", argv);
+const invokeAction = async ({action, id, name, email, phone}) => {
+  switch (action) {
+    case "list":
+      const contacts = await listContact();
+      console.table(contacts);
+      break;
 
-//     default:
-//       console.warn("\x1B[31m Unknown action type!");
-//   }
-// }
+    case "get":
+      const contactById = await getContactById(id);
 
-// invokeAction(argv);
+      if (contactById) {
+        console.log(chalk.red.greenBright.bold("Contact found!"));
+        console.table(contactById);
+      } else {
+        console.log(chalk.yellow.greenBright.bold("Contact not found!"));
+      }
+      break;
+
+    case "add":
+      const contact = await addContact(name, email, phone);
+      console.log(chalk.blue.greenBright.bold("Add new contact!"));
+      console.table(contact);
+      break;
+
+    case "remove":
+      const deleteContact = await removeContact(id);
+      if (deleteContact) {
+        console.table(deleteContact);
+      } else {
+        console.log(chalk.yellow("Incorrect id. Try again, please!"));
+      }
+      break;
+
+    default:
+      console.warn(chalk.red("Unknown action type!"));
+  }
+};
+
+invokeAction(argv).then(() =>
+  console.log(chalk.green.bgYellow.bold("Operation success"))
+);
